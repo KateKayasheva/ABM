4989e340234d6765a98f9d03273cba0ef5529fd9
Kate <evkayasheva@gmail.com >
2021-05-13 16:09:00 +0300

    agent_id is added to the order dictionary

--- Agent.py
+++ Agent.py
@@ -1,11 +1,13 @@
 import random
 
+
 class Agent:
     """
     Agent class
     - money (float) amount of money in the pocket
     - stocks (int) number of stocks owned
     """
+
     def __init__(self, money, stocks):
         self.money = money
         self.stocks = stocks
@@ -13,9 +15,12 @@ class Agent:
 
     def wealth(self):
         return "Agent owns {} stocks and {} of money".format(self.stocks, self.money)
-    def __str__(self):
-        return "Agent is of type {}".format(self.type)
 
+    def __str__(self):
+        return "Agent is of type {}, id: {}".format(self.type, id(self))
+
+    # def __str__(self):
+    #     return "<Agent %s>" % id(self)
 
     def record(self, direction, price, quantity):
         """
@@ -23,63 +28,54 @@ class Agent:
         """
         if direction == "SELL":
             self.stocks -= quantity
-            self.money += quantity*price
+            self.money += quantity * price
         elif direction == "BUY":
             self.stocks += quantity
-            self.money -= quantity*price
+            self.money -= quantity * price
 
 
 class RandomTrader(Agent):
     """
     Random trader
     """
+
     def __init__(self, money, stocks):
         Agent.__init__(self, money, stocks)
 
         self.type = "RANDOM"
 
+    def order(self):
+        """
+        Set the order
+        BUY if no stocks on hand
+        If stocks > 0 choose direction at random
+        order format dictionary as {'direction': direction, 'price': price, 'quantity': quantity, 'agent': id(self)}
 
-   def order(self):
-       """
-       Set the order
-       BUY if no stocks on hand
-       If stocks > 0 choose direction at random
-       order format dictionary as {'direction': direction, 'price': price, 'quantity': quantity}
+        """
+        # Determine the direction
+        if self.stocks > 0:
+            direction = random.choice(("BUY", "SELL"))
+        else:
+            direction = "BUY"
 
-       """
-       # Determine the direction
-       if self.stocks > 0:
-           direction = random.choice(("BUY", "SELL"))
-       else:
-           direction = "BUY"
+        #  Determine price
+        # TODO: determine the price
+        # Max price per stock is all the money
 
-       #  Determine price
-       # TODO: determine the price
-       price = 1
+        price = random.randint(1, self.money*100)/100
 
-       # Determine quantity
-       if direction == "SELL":
-           maxq = self.stocks
-           try:
-               quantity = random.randint(1, maxq)
-           except ValueError:
-               quantity = 1
+        # Determine quantity
+        if direction == "SELL":
+            maxq = self.stocks
+            try:
+                quantity = random.randint(1, maxq)
+            except ValueError:
+                quantity = 1
+        elif direction == "BUY":
+            quantity = random.randint(1, int(self.money / price))  # TODO: find the lower bound (seems fine)
 
-
-       elif direction == "BUY":
-           quantity = random.randint(1, int(self.money/price)) # TODO: find the lower bound
-
-
-       order = {'direction': direction, 'price': price, 'quantity': quantity}
-
-       return order
+        order = {'direction': direction, 'price': price, 'quantity': quantity, 'agent': id(self)}
+        return order
 
 
-
-
-
-
-
-
-
-print(RandomTrader(1,2).wealth())
+print(RandomTrader(1, 2))
