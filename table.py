c5e7f178617247027412a0f8a6d6b6e415e2c9ee
Kate <evkayasheva@gmail.com >
2021-06-09 14:19:05 +0300

    Plot generation for 7% change added

--- table.py
+++ table.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('TestData2/test_2.csv')
+test = 'Test_5'
+df = pd.read_csv('TestData/test_5.csv')
 print(df)
 last = df.sort_values('TIME').groupby('DAY').tail(1)
 print(last)
@@ -10,63 +11,80 @@ pct0 = last['PRICE'].pct_change()
 pct = pd.DataFrame()
 
 pct['DAY'] = last['DAY']
-pct['PRICE'] = pct0
+pct['PRICE'] = pct0 * 100
 print(pct)
 print(pct[pct['PRICE'] == 0].iloc[0]['DAY'])
 # first zero change day
-
 d0 = int(pct[pct['PRICE'] == 0].iloc[0]['DAY'])
-l = 'Zero price change at day ' + str(d0)
-
-d = pct[pct['PRICE'] == 0]['DAY']
-
-days = []
-p = 0
-for i in d:
-    if p + 1 != i:
-        p = i
-        days.append(i)
-    else:
-        p = i
 
 
-top = plt.subplot2grid((6,8), (0, 0), rowspan=3, colspan=8)
+def price_change(c, e=1):
+    if e == 1:
+        d = pct[pct['PRICE'] == c]['DAY']
+    elif e == 0:
+        d = pct[pct['PRICE'] >= c]['DAY']
+
+    days = []
+    p = 0
+    for i in d:
+        if p + 1 != i:
+            p = i
+            days.append(i)
+        else:
+            p = i
+    return days
+
+
+days0 = price_change(0)
+days7 = price_change(7, e=0)
+
+top = plt.subplot2grid((6, 8), (0, 0), rowspan=3, colspan=8)
 top.plot(last["DAY"], last["PRICE"], c='navy')
-for i in days:
-    l = 'Zero price change at day ' + str(i)
+for i in days0:
+    l = str(0) + '% price change at day ' + str(i)
     top.axvline(i, c='maroon', label=l)
+
+for i in days7:
+    l = str(7) + '% price change at day ' + str(i)
+    top.axvline(i, c='darkgreen', label=l, ls='--')
+
 top.legend()
 plt.xlabel('DAY')
 plt.ylabel('PRICE')
 
-plt.title('TEST_2 Prices')
+plt.title('{} Prices'.format(test))
 
-bottom = plt.subplot2grid((6,8), (3,0), rowspan=1, colspan=8)
+bottom = plt.subplot2grid((6, 8), (3, 0), rowspan=1, colspan=8)
 bottom.bar(last["DAY"], last['Q'], color='navy')
-for i in days:
-    l = 'Zero price change at day ' + str(i)
+
+for i in days0:
+    l = '0% price change at day ' + str(i)
     bottom.axvline(i, c='maroon', label=l)
+
+for i in days7:
+    l = str(7) + '% price change at day ' + str(i)
+    bottom.axvline(i, c='darkgreen', label=l, ls='--')
 
 plt.xlabel('DAY')
 plt.ylabel('Volume (QUANTITY)')
-plt.title('TEST_2 Trading Volume')
+plt.title('{} Trading Volume'.format(test))
 
-bottom2 = plt.subplot2grid((6,8), (4,0), rowspan=2, colspan=8)
+bottom2 = plt.subplot2grid((6, 8), (4, 0), rowspan=2, colspan=8)
 bottom2.plot(pct["DAY"], pct['PRICE'], c='navy')
-for i in days:
-    l = 'Zero price change at day ' + str(i)
+
+for i in days0:
+    l = '0% price change at day ' + str(i)
     bottom2.axvline(i, c='maroon', label=l)
 
-plt.title('TEST_2 Price change')
+for i in days7:
+    l = str(7) + '% price change at day ' + str(i)
+    bottom2.axvline(i, c='darkgreen', label=l, ls='--')
+
+plt.title('{} Price change'.format(test))
 plt.xlabel('DAY')
-plt.ylabel('Percentage price change')
+plt.ylabel('% price change')
 
-
-plt.gcf().set_size_inches(15,10)
-
+plt.gcf().set_size_inches(15, 10)
 
 plt.tight_layout(pad=3.0)
 plt.show()
-
-
-
